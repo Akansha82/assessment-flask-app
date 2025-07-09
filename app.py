8c09236baab9884c623881e023154b8893d14a92
Yadav <503433990@ge.com>
2025-07-09 19:36:25 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name">
+            <input type="submit">
+        </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
