ff0641fddded551f35c29a7bde6b9904c4f79132
Yadav <503433990@ge.com>
2025-07-15 18:42:13 +0530

    Added password field

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         name = request.form['name']
-        return f'Hello, {name}!'
+        password = request.form['password']
+        return f'Hello, {name}! Your password is: {password}'
     return '''
         <form method="post">
-            Name: <input type="text" name="name">
+            Name: <input type="text" name="name"><br>
+            Password: <input type="password" name="password"><br>
             <input type="submit">
         </form>
     '''
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
